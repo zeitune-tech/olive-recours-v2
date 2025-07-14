85a43c330a879224acf5ca15fc36d7c294a30fa9
Mouhamadou Tidiane Seck <mtseckdev@gmail.com>
2025-07-14 12:30:47 +0000

    fix env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
-  base_url: "https://zeitune.lhacksrt.tech/api/v1",
+  base_url: "https://apirecours.zeitune.tech/api/v1",
+  request_url: "https://apirecours.zeitune.tech/api/v1/app",
   production: true
 };
